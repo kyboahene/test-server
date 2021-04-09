8e2eb38f35342e57ed5da0c4dc19ced5e9b25a85
kyboahene <kyeiyaw437@gmail.com>
2021-04-09 12:18:34 -0800

    little changes

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,83 +1,99 @@
-const  { gql } = require("apollo-server-express");
+const { gql } = require('apollo-server-express')
 
 module.exports = gql`
-    type Query{
-        getUsers: [User]
-        getProspectsPerUsername(username: String!): [Prospect]
-        getAllProspects: [Prospect]
-    }
+  type Query {
+    getUsers: [User]
+    getProspectsPerUsername(username: String!): [Prospect]
+    getAllProspects: [Prospect]
+  }
 
-    type Mutation{
-        register(registerInput: registerInput): User!
-        login(email: String!, password: String! ): User!
-        sendForgotPasswordEmail(email: String!): User!
-        forgotPasswordChange(newPassword: String!, key: String!): User!
+  type Mutation {
+    register(registerInput: registerInput): User!
+    login(email: String!, password: String!): User!
+    updateUser(
+      userID: ID
+      username: String
+      email: String
+      password: String
+      confirmPassword: String
+    ): User!
+    sendForgotPasswordEmail(email: String!): Link!
+    forgotPasswordChange(newPassword: String!, key: String!): User!
 
-        addProspect(prospectDetails: prospectDetails): Prospect!
-        deleteProspect(ProspectId: ID!): String!
-        addDriverLincense(ProspectId: ID!, DofBirth: String!, Number: String!, State: State!): Prospect!
-        addProvider(ProspectId: String!, Provider: Providers!): Prospect!
-    }
+    addProspect(prospectDetails: prospectDetails): Prospect!
+    deleteProspect(ProspectId: ID!): String!
+    addDriverLincense(
+      ProspectId: ID!
+      DofBirth: String!
+      Number: String!
+      State: State!
+    ): Prospect!
+    addProvider(ProspectId: String!, Provider: Providers!): Prospect!
+  }
 
-    type User{
-        id: ID!,
-        email: String!,
-        username: String!,
-        token: String!,
-        createdAt: String!
-    }
+  type Link {
+    link: String!
+  }
 
-    type Prospect{
-        id: ID!,
-        fname: String!,
-        Lname: String!,
-        email: String!,
-        phone: String!,
-        provider: String,
-        status: String! 
-        driver_license: [Driver_Info]
-        createdAt: String!
-        user: String!
-    } 
+  type User {
+    id: ID!
+    email: String!
+    username: String!
+    token: String!
+    createdAt: String!
+  }
 
-    type Driver_Info{
-        DofBirth: String!,
-        Number: String!,
-        State: String!
-    }
+  type Prospect {
+    id: ID!
+    fname: String!
+    Lname: String!
+    email: String!
+    phone: String!
+    provider: String
+    status: String!
+    driver_license: [Driver_Info]
+    createdAt: String!
+    user: String!
+  }
 
+  type Driver_Info {
+    DofBirth: String!
+    Number: String!
+    State: String!
+  }
 
-    input registerInput{
-        username: String!,
-        password: String!,
-        confirmPassword: String!,
-        email: String!
-    }
+  input registerInput {
+    username: String!
+    password: String!
+    confirmPassword: String!
+    email: String!
+  }
 
-    input prospectDetails{
-        username: String!,
-        fname: String!,
-        Lname: String!,
-        email: String!,
-        phone: String!,
-    }
+  input prospectDetails {
+    username: String!
+    fname: String!
+    Lname: String!
+    email: String!
+    phone: String!
+  }
 
-    enum Providers{
-        AllState
-        StateFarm
-        Geico
-        USaa
-        farmers Insurance
-        Kemper
-        Progressive
-    }
+  enum Providers {
+    AllState
+    StateFarm
+    Geico
+    USaa
+    farmers
+    Insurance
+    Kemper
+    Progressive
+  }
 
-    enum State{
-        California
-        Colorado
-        Kumasi 
-        Accra
-        Konongo
-        Swedru
-    }
-`;
+  enum State {
+    California
+    Colorado
+    Kumasi
+    Accra
+    Konongo
+    Swedru
+  }
+`
