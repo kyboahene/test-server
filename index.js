faf936b9dfc04539f3b39828f535a7f97701b87e
kyboahene <kyeiyaw437@gmail.com>
2021-04-12 15:24:40 -0800

    changed port

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
-const { ApolloServer }  = require("apollo-server-express");
-const express = require("express");
-const mongoose = require("mongoose");
+const { ApolloServer } = require('apollo-server-express')
+const express = require('express')
+const mongoose = require('mongoose')
 
-const { MONGODB, PORT } = require("./config");
-const typeDefs = require("./graphql/typeDefs");
-const resolvers = require("./graphql/resolvers");
+const { MONGODB, PORT } = require('./config')
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers')
 
-
-mongoose.set("useFindAndModify", false);
+mongoose.set('useFindAndModify', false)
 
 const startServer = async () => {
-    await mongoose.connect(MONGODB || "mongodb://localhost:27018/insurerity", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-    }).then(() => {
-        console.log("Mongodb connected");
-        app.listen(PORT, () => {
-            console.log(`Server is connected${server.graphqlPath}`);
-        })
-    }).catch((err) => {
-        console.error(err);
+  await mongoose
+    .connect(MONGODB || 'mongodb://localhost:27018/insurerity', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    .then(() => {
+      console.log('Mongodb connected')
+      app.listen(PROCESS.ENV.PORT, () => {
+        console.log(`Server is connected${server.graphqlPath}`)
+      })
+    })
+    .catch((err) => {
+      console.error(err)
     })
 }
 
-const app = express();
+const app = express()
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers,
 })
 
+server.applyMiddleware({ app })
 
-server.applyMiddleware({app});
-
-startServer();
-
-
-
+startServer()
